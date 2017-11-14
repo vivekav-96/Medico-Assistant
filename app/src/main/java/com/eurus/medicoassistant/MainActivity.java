ff987a3efc3c414aee92b5713351456ddb8ed3d3
bineeshpbabu <bineesh@creopedia.com>
2017-11-15 00:29:13 +0530

    Updated dashboard

--- app/src/main/java/com/eurus/medicoassistant/MainActivity.java
+++ app/src/main/java/com/eurus/medicoassistant/MainActivity.java
@@ -1,11 +1,15 @@
 package com.eurus.medicoassistant;
 
 import android.content.Intent;
+import android.support.annotation.IdRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnTabSelectListener;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton fab_book_app;
@@ -14,8 +18,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        fab_book_app = (FloatingActionButton)findViewById(R.id.fab_book_app);
+        setContentView(R.layout.dashboard);
+        BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        fab_book_app = findViewById(R.id.fab_action);
         fab_book_app.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -23,8 +28,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
-       /* toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("Dashboard");
-        setSupportActionBar(toolbar);*/
+        bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
+            @Override
+            public void onTabSelected(@IdRes int tabId) {
+                if (tabId == R.id.tab_list1) {
+                    // The tab with id R.id.tab_favorites was selected,
+                    // change your content accordingly.
+                }
+                else if(tabId == R.id.tab_list2){
+                }
+            }
+        });
     }
 }
