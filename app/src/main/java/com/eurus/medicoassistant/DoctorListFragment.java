6e116b0cfdf6a8d13f2e07ae7993d8655a5f223c
Vivek Av <vivekav-96@users.noreply.github.com>
2017-11-19 18:27:05 +0530

    Fixed doctor fragment issue

--- app/src/main/java/com/eurus/medicoassistant/DoctorListFragment.java
+++ app/src/main/java/com/eurus/medicoassistant/DoctorListFragment.java
@@ -1,11 +1,9 @@
 package com.eurus.medicoassistant;
 
-import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,36 +26,22 @@ public class DoctorListFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        doctorsListView=getActivity().findViewById(R.id.doctorsListView);
-        doctorDetails=new DoctorDetails();
-        doctorsList=doctorDetails.getDoctorDetails();
-        listAdapter=new DoctorsListAdapter(getActivity(), doctorsList);
-        doctorsListView.setAdapter(listAdapter);
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
 
         ((MainActivity)getActivity()).getTabLayout().setVisibility(View.GONE);
-        return inflater.inflate(R.layout.activity_doctor_list, container, false);
+        return inflater.inflate(R.layout.fragment_doctor_list, container, false);
     }
-    /* @Override
-    protected void onResume() {
-        super.onResume();
-        doctorDetails=new DoctorDetails();
-        doctorsList=doctorDetails.getDoctorDetails();
-        listAdapter=new DoctorsListAdapter(DoctorListFragment.this, doctorsList);
-        doctorsListView.setAdapter(listAdapter);
-    }*/
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
+        doctorsListView= view.findViewById(R.id.doctorsListView);
+        doctorDetails=new DoctorDetails();
+        doctorsList=doctorDetails.getDoctorDetails();
+        listAdapter=new DoctorsListAdapter(getActivity(), doctorsList);
+        doctorsListView.setAdapter(listAdapter);
         //progressBar=findViewById(R.id.doctorsListProgressBar);
         // progressBar.setVisibility(View.VISIBLE);
 
